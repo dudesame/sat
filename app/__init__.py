d3f58ae9692bacaed8e401771e941b0cfcec29ae
dudesame <36504183+dudesame@users.noreply.github.com>
2020-06-03 12:28:28 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=Config.TEMPLATES_DIR)
 wsgi_app = app.wsgi_app
 app.config.from_object(Config)
 
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+project_dir=os.path.join(basedir, 'app')
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 25
+    TEMPLATES_DIR= os.path.join(project_dir, 'Templates')
